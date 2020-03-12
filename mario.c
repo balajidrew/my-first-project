959b429566a1bb64ebeffffab52380d269af6f24
balajidrew <60146164+balajidrew@users.noreply.github.com>
2020-03-12 10:21:16 -0400

    Create mario.c

---
+++ mario.c
@@ -0,0 +1,21 @@
+int main(void)
+{
+    int height;
+    do
+    {
+    height = get_int("How high do you want your pyramid? ");
+    }
+    while (height < 1);
+    for (int i = 0; i < height; i++)
+    {
+        for(int y = 1; y < (height - i) ; y++)
+        {
+            printf(" ");
+        }
+        for (int x = 0; x < i; x++ )
+        {
+            printf("#");
+        }
+        printf("#\n");
+    }
+    
